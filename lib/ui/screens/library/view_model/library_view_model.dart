991ff2a400be45d8438e2fba4a90b24cb2da8f06
TET ELITE <5hibalord69@gmail.com>
2026-03-04 22:42:49 +0700

    001 -- Implement LibraryViewModel and LibraryContent for improved state management in LibraryScreen

---
+++ lib/ui/screens/library/view_model/library_view_model.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../../data/repositories/songs/song_repository.dart';
+import '../../../../model/songs/song.dart';
+import '../../../states/player_state.dart';
+
+class LibraryViewModel extends ChangeNotifier {
+  final SongRepository _songRepository;
+  final PlayerState _playerState;
+
+  List<Song> _songs = [];
+
+  LibraryViewModel({
+    required SongRepository songRepository,
+    required PlayerState playerState,
+  })  : _songRepository = songRepository,
+        _playerState = playerState {
+    _playerState.addListener(_onPlayerStateChanged);
+  }
+
+  void init() {
+    _songs = _songRepository.fetchSongs();
+    notifyListeners();
+  }
+
+  void _onPlayerStateChanged() {
+    notifyListeners();
+  }
+
+  List<Song> get songs => _songs;
+
+  Song? get currentSong => _playerState.currentSong;
+
+  bool isPlaying(Song song) => _playerState.currentSong == song;
+
+  void play(Song song) {
+    _playerState.start(song);
+  }
+
+  void stop() {
+    _playerState.stop();
+  }
+
+  @override
+  void dispose() {
+    _playerState.removeListener(_onPlayerStateChanged);
+    super.dispose();
+  }
+}
